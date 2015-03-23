8ff012cec59a5c8c30bf3a66fa69d603c6ca588a
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2015-03-23 11:24:49 +0000

    Fixed mistakenly redefine global variable in task_02 and removed whitespaces.

--- task_02.py
+++ task_02.py
@@ -8,15 +8,15 @@ import getpass
 
 def login(username, maxattempts=3):
     """This function takes input from a user and checks the password.
-       
+
        Arg:
            username(str): String input from user.
            maxattempts(int): Max attempts for login.
-           
+
         Return:
             auth(boolean): True or False if user successfully authenticated
             before hitting maximum no. of failed attempts.
-            
+
         Examples:
                 >>>login('mike', 4)
                     Incorrect username or password. You have 4 attempts.
@@ -25,15 +25,14 @@ def login(username, maxattempts=3):
                     Incorrect username or password. You have 1 attempts.
                     Incorrect username or password. You have 0 attempts.
                     False
-
     """
 
     auth = False
-    login = 'Please enter your password: '
+    user_login = 'Please enter your password: '
     auth_fail = "Incorrect username or password. You have" ' {} '  "attempts."
     attempt = 1
     while attempt <= maxattempts:
-        passwd = getpass.getpass(login)
+        passwd = getpass.getpass(user_login)
         message = authentication.authenticate(username, passwd)
         if message:
             auth = True
